5f6ac9f9d6495160dc6428757064961787a914f2
developeasusual <kartik13rer@gmail.com>
2025-02-08 19:24:04 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: "/developeasusual",
+  base: "/developeasusual.github.io",
   plugins: [react()]
 })
