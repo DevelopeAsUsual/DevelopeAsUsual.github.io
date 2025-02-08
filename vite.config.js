acd5b7d6006451cccad47549b864d969912f8d91
developeasusual <kartik13rer@gmail.com>
2025-02-08 19:31:20 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: "/developeasusual.github.io",
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 })
