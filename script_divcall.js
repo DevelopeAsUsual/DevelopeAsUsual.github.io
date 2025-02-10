7d1bf3be7470caad252e01b4776c6a240fa3bc7f
DevelopeAsUsual <kartik13rer@gmail.com>
2025-02-10 19:16:54 +0530

    update

--- script_divcall.js
+++ script_divcall.js
@@ -1,6 +1,43 @@
 fetch('src/About.html')
 .then(response => response.text())
 .then(data => {
-    document.getElementById('box-container').innerHTML = data;
+    document.getElementById('About').innerHTML = data;
 })
 .catch(error => console.error('Error loading the div:', error));
+
+
+fetch('src/Navbar.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('Navbar').innerHTML = data;
+})
+.catch(error => console.error('Error loading the div:', error));
+
+fetch('src/Education.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('Education').innerHTML = data;
+})
+.catch(error => console.error('Error loading the div:', error));
+
+fetch('src/Work.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('Work').innerHTML = data;
+})
+.catch(error => console.error('Error loading the div:', error));
+
+fetch('src/Project.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('Project').innerHTML = data;
+})
+.catch(error => console.error('Error loading the div:', error));
+
+fetch('src/Contact.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('Contact').innerHTML = data;
+})
+.catch(error => console.error('Error loading the div:', error));
+
